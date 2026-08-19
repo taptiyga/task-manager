271269f9a2b4512196d8c48c6718e6fb5b6179b6
Taptiyga1 <taptiyga1@gmail.com>
2026-08-19 14:14:30 +0400

    Impliment task editing

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -4,10 +4,16 @@ import { TaskItem } from "./TaskItem";
 type TaskListProps = {
   tasks: Task[];
   deleteTask: (id: string) => void;
+  updateTask: (id: string, title: string, description: string) => void;
 };
 
-export function TaskList({ tasks, deleteTask }: TaskListProps) {
+export function TaskList({ tasks, deleteTask, updateTask }: TaskListProps) {
   return tasks.map((task) => (
-    <TaskItem key={task.id} task={task} deleteTask={deleteTask} />
+    <TaskItem
+      key={task.id}
+      task={task}
+      deleteTask={deleteTask}
+      updateTask={updateTask}
+    />
   ));
 }
